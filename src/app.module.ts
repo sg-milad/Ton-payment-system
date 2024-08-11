32b4a0e34c1ccdd362ad434f144a878ba71d3c5e
milad <miladbachari14@gmail.com>
2024-08-11 16:16:49 +0330

    feat: add wallet module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigModuleOptions } from './configs/config.module.options';
 import { PrismaModule } from './shared/prisma/prisma.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(ConfigModuleOptions()),
-    PrismaModule
+    PrismaModule,
+    WalletModule
   ],
   controllers: [AppController],
   providers: [AppService],

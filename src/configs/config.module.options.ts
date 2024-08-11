18df7fb20df353e8a1d65933ae9cb79c1ee7742a
milad <miladbachari14@gmail.com>
2024-08-11 16:15:46 +0330

    feat: add wallet config

--- src/configs/config.module.options.ts
+++ src/configs/config.module.options.ts
@@ -1,11 +1,12 @@
 import { productionEnv } from "src/shared/helper/helper";
 import { ConfigModuleOptions } from "@nestjs/config";
+import walletConfig from "./wallet.config";
 
 export function ConfigModuleOptions(): ConfigModuleOptions {
     const options: ConfigModuleOptions = {};
     options.isGlobal = true;
     options.cache = true;
-    options.load = [];
+    options.load = [walletConfig];
     if (!productionEnv()) {
         options.envFilePath = `.env.${process.env.NODE_ENV}`;
     }

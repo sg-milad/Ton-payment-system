0e06e67ab6b2f4a93b6f9a2479be49332edb38d1
milad <miladbachari14@gmail.com>
2024-08-16 18:56:53 +0330

    feat: process transaction in ton service

--- src/ton/ton.service.ts
+++ src/ton/ton.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { getHttpEndpoint } from "@orbs-network/ton-access";
-import { Address, TonClient, } from "@ton/ton";
+import { Address, fromNano, TonClient, } from "@ton/ton";
 import { PaymentService } from "src/payment/payment.service";
 import { WalletService } from "src/wallet/wallet.service";
 import TonWeb from "tonweb";
@@ -19,30 +19,52 @@ export class TonService implements OnModuleInit {
         const endpoint = await getHttpEndpoint();
         this.client = new TonClient({ endpoint });
         this.tonWeb = new TonWeb();
+        await this.transactionProcess()
     }
-    async checkTransactions(address: Address) {
+    async checkTransaction(address: Address): Promise<{ value: bigint; source: Address, tx: string } | null> {
         try {
             const transactions = await this.client.getTransactions(address, {
                 limit: 2,
             });
             if (transactions.length > 0) {
                 for (const tx of transactions) {
-                    console.log(tx);
+                    if (tx.inMessage && tx.inMessage.info.type === 'internal') {
+                        const value = tx.inMessage.info.value.coins;
+                        const source = tx.inMessage.info.src;
+                        return {
+                            value,
+                            source,
+                            tx: tx.hash().toString("hex")
+                        }
+                    }
+                    return null
                 }
             }
-
+            return null
         } catch (error) {
             console.error("Error checking transactions:", error);
+            return null
         }
     }
-    async getWalletsAddress(): Promise<Address[]> {
+    async transactionProcess(): Promise<void> {
         const getInitWallets = await this.PaymentService.findInitPayment()
-        let walletAddress: Address[] = []
+        if (getInitWallets.length === 0) return;
         for (const wallet of getInitWallets) {
             const address = await this.walletService.createWallet(wallet.walletAccount, 0, wallet.walletIndex)
-            walletAddress.push(address.publicKey)
-        }
-        return walletAddress
-    }
+            const transaction = await this.checkTransaction(address.publicKey)
+            console.log(address.publicKey);
+            console.log(transaction);
 
+            if (transaction) {
+                await this.PaymentService
+                    .updatePayment({ id: wallet.id },
+                        {
+                            tx: transaction.tx,
+                            balance: Number(fromNano(transaction.value)),
+                            walletSrc: transaction.source.toString(),
+                            paymentStatus: "ACCEPT"
+                        })
+            }
+        }
+    }
 }

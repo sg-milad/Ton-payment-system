bad9110a91e4de8f93c4c852504a2fd7045cfedb
milad <miladbachari14@gmail.com>
2024-08-16 18:54:52 +0330

    refactor: remove account and change

--- src/configs/wallet.config.ts
+++ src/configs/wallet.config.ts
@@ -4,6 +4,4 @@ import * as process from "process";
 export default registerAs("wallet", () => ({
     mnemonic: process.env.WALLET_MNEMONIC,
     passphrase: process.env.WALLET_SEED_PASSPHRASE,
-    account: process.env.WALLET_ACCOUNT,
-    change: process.env.WALLET_CHANGE,
 }));

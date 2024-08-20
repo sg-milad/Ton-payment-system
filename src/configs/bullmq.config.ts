770d8e41787dd014053f53e6d02741646ac00a41
milad <miladbachari14@gmail.com>
2024-08-20 23:23:19 +0330

    feat: add bull config

---
+++ src/configs/bullmq.config.ts
@@ -0,0 +1,16 @@
+import { BullRootModuleOptions, SharedBullConfigurationFactory } from "@nestjs/bullmq";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class BullConfigService implements SharedBullConfigurationFactory {
+    constructor(private configService: ConfigService) { }
+    createSharedConfiguration(): BullRootModuleOptions {
+        return {
+            connection: {
+                host: this.configService.get("redis.host"),
+                port: this.configService.get("redis.port"),
+            },
+        };
+    }
+}

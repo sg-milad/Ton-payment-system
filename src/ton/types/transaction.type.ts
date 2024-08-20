7a36beb5d671b31a98da68f6799eacf9eaa4280c
milad <miladbachari14@gmail.com>
2024-08-20 23:27:42 +0330

    feat: add  transaction type

---
+++ src/ton/types/transaction.type.ts
@@ -0,0 +1,8 @@
+import { Payment } from "@prisma/client";
+
+export type TransactionType = { value: string; source: string, tx: string }
+
+export type TransactionPaymentJob = {
+    transaction: TransactionType,
+    payment: Payment,
+}

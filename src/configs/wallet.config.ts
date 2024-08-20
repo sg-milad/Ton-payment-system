2f714d1f1c0447c2915fcf84a1c445c17a9c6bc2
milad <miladbachari14@gmail.com>
2024-08-20 23:24:08 +0330

    feat: add main wallet to config

--- src/configs/wallet.config.ts
+++ src/configs/wallet.config.ts
@@ -4,4 +4,5 @@ import * as process from "process";
 export default registerAs("wallet", () => ({
     mnemonic: process.env.WALLET_MNEMONIC,
     passphrase: process.env.WALLET_SEED_PASSPHRASE,
+    mainWallet: process.env.MAIN_WALLET
 }));

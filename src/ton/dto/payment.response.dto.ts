fb12d26755e9abf81b10184a1ddefe8a3d2b4bc0
milad <miladbachari14@gmail.com>
2024-08-20 23:38:03 +0330

    feat: payment response  dto

---
+++ src/ton/dto/payment.response.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { PaymentStatus } from '@prisma/client';
+
+export class PaymentResponseDto {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty({ enum: PaymentStatus })
+    paymentStatus: string;
+
+    @ApiProperty()
+    walletAccount: number;
+
+    @ApiProperty()
+    walletIndex: number;
+
+    @ApiPropertyOptional({ required: false })
+    balance?: string;
+
+    @ApiPropertyOptional({ required: false })
+    walletSrc?: string;
+
+    @ApiPropertyOptional({ required: false })
+    walletDes?: string;
+
+    @ApiPropertyOptional({ required: false })
+    tx?: string;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+}

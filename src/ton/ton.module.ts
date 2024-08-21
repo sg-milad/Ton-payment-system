a4074e52283efd34d15289984b3dcac61438a3e9
milad <miladbachari14@gmail.com>
2024-08-21 13:23:37 +0330

    feat: implement ton module

--- src/ton/ton.module.ts
+++ src/ton/ton.module.ts
@@ -2,9 +2,19 @@ import { Module } from "@nestjs/common";
 import { TonService } from "./ton.service";
 import { WalletModule } from "src/wallet/wallet.module";
 import { PaymentModule } from "src/payment/payment.module";
+import { BullModule } from "@nestjs/bullmq";
+import { PaymentQUEUE } from "./constants/payment.bull.costant";
+import { TransactionConsumer } from "./queues/transaction.queue";
 
 @Module({
-    imports: [WalletModule, PaymentModule],
-    providers: [TonService]
+    imports: [
+        BullModule.registerQueue({
+            name: PaymentQUEUE,
+        }),
+        WalletModule,
+        PaymentModule
+    ],
+    providers: [TonService, TransactionConsumer],
+    exports: [TonService]
 })
 export class TonModule { }

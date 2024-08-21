e1cb609ce73aba0b5a6de75aabe69fd93edbcf21
milad <miladbachari14@gmail.com>
2024-08-21 13:24:45 +0330

    feat: add logger to ton service

--- src/ton/ton.service.ts
+++ src/ton/ton.service.ts
@@ -1,5 +1,5 @@
 import { InjectQueue } from "@nestjs/bullmq";
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
 import { getHttpEndpoint } from "@orbs-network/ton-access";
 import { Address, fromNano, internal, SendMode, toNano, TonClient, WalletContractV3R1 } from "@ton/ton";
@@ -12,6 +12,7 @@ import { ConfigService } from "@nestjs/config";
 import { KeyPair } from "@ton/crypto";
 @Injectable()
 export class TonService implements OnModuleInit {
+    private readonly logger = new Logger(TonService.name);
     private client: TonClient
 
     constructor(
@@ -22,36 +23,46 @@ export class TonService implements OnModuleInit {
     ) { }
 
     async onModuleInit() {
-        const endpoint = await getHttpEndpoint({ network: "mainnet" })
-        this.client = new TonClient({ endpoint });
+        try {
+            const isMainNet = this.configService.get("app.isMainnet")
+            const endpoint = await getHttpEndpoint({ network: isMainNet === "true" ? "mainnet" : "testnet" })
+
+            this.client = new TonClient({ endpoint });
+        } catch (error) {
+            this.logger.error('Failed to initialize TonClient', error);
+        }
     }
 
     @Cron(CronExpression.EVERY_MINUTE)
     async transactionProcess(): Promise<void> {
         const getInitPayment = await this.PaymentService.findInitPayment()
-
+        this.logger.log("find init Payment...", getInitPayment)
         if (getInitPayment.length === 0) return;
         for (const payment of getInitPayment) {
             const wallet = await this.walletService.createWallet(payment.walletAccount, 0, payment.walletIndex)
             const publicKey = await this.walletService.keyPairToPublicKey(wallet)
             const transactions = await this.checkTransactions(publicKey)
             for (const transaction of transactions) {
+                this.logger.log("Transactions", { publicKey, transaction })
                 await this.paymentQueue.add(PaymentJob, { transaction, payment }, { removeOnComplete: true })
             }
 
         }
     }
     @Cron(CronExpression.EVERY_MINUTE)
-    async checkPendingPayment() {
+    async checkPendingPayments() {
         const getPendingPayment = await this.PaymentService.findPendingPayment()
+        this.logger.log("Check pending payments", getPendingPayment)
         for (const payment of getPendingPayment) {
             if (payment.walletDes) {
                 const balance = await this.getBalance(payment.walletDes)
-                if (balance > toNano("0.01")) {
+                if (balance > toNano("0.1")) {
+                    this.logger.log("Transfer remaining balance", { balance: toNano(balance), payment })
                     const wallet = await this.walletService.createWallet(payment.walletAccount, 0, payment.walletIndex)
                     return await this.transfer(wallet)
                 }
                 await this.PaymentService.updatePayment({ id: payment.id }, { paymentStatus: "ACCEPT" })
+                this.logger.log("Payment accepted", payment)
             }
         }
     }
@@ -60,6 +71,7 @@ export class TonService implements OnModuleInit {
             const transactions = await this.client.getTransactions(Address.parse(publicKey), {
                 limit: 5,
             });
+            this.logger.log("Check transactions", { publicKey, transactions })
             const results: Array<TransactionType> = [];
 
             for (const tx of transactions) {
@@ -77,22 +89,15 @@ export class TonService implements OnModuleInit {
 
             return results;
         } catch (error) {
-            console.error("Error checking transactions:", error);
+            this.logger.error("Failed to get transactions", { publicKey, error })
             return [];
         }
-    }
-    async generatePayment() {
-        const payment = await this.PaymentService.createPayment()
-        const wallet = await this.walletService.createWallet(payment.walletAccount, 0, payment.walletIndex)
-        const publicKey = await this.walletService.keyPairToPublicKey(wallet)
-        return { publicKey: publicKey }
     }
     async transfer(keyPair: KeyPair) {
         try {
             const wallet = WalletContractV3R1.create({ publicKey: keyPair.publicKey, workchain: 0 })
             let contract = this.client.open(wallet);
             let seqno = await contract.getSeqno();
-
             return await contract.sendTransfer({
                 seqno,
                 secretKey: keyPair.secretKey,
@@ -106,13 +111,10 @@ export class TonService implements OnModuleInit {
                 sendMode: SendMode.CARRY_ALL_REMAINING_BALANCE
             })
         } catch (error) {
-            throw error;
+            this.logger.error("Failed to transfer Ton", { error })
         }
     }
     async getBalance(publicKey: string): Promise<bigint> {
         return await this.client.getBalance(Address.parse(publicKey))
-    }
-    async isWalletDeployed(publicKey: string): Promise<boolean> {
-        return await this.client.isContractDeployed(Address.parse(publicKey))
     }
 }

242e783edfb5ce4aa6d4a0ae8ef5a865fe0c380c
milad <miladbachari14@gmail.com>
2024-08-21 13:07:01 +0330

    feat: add services to payment module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { PaymentService } from "./payment.service";
+import { WalletModule } from "src/wallet/wallet.module";
 @Module({
+    imports: [WalletModule],
     providers: [PaymentService],
-    exports: [PaymentService]
+    exports: [PaymentService],
 })
 export class PaymentModule { }

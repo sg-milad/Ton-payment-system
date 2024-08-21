2e9893e35be4dcddfb9872e8cbdb3b23e2055f33
milad <miladbachari14@gmail.com>
2024-08-21 13:24:59 +0330

    feat: extend CreatePaymentResponse

--- src/ton/dto/create.payment.response.dto.ts
+++ src/ton/dto/create.payment.response.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { PaymentResponseDto } from "./payment.response.dto";
 
-export class CreatePaymentResponse {
+export class CreatePaymentResponse extends PaymentResponseDto {
     @ApiProperty({ type: String })
     publicKey: string
 }

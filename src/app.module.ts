319cd380dd6aab342fe38c71ab3f04b770cafec9
milad <miladbachari14@gmail.com>
2024-08-21 13:06:26 +0330

    feat: add modules to module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModuleOptions } from './configs/config.module.options';
 import { PrismaModule } from './shared/prisma/prisma.module';
+import { BullConfigService } from './configs/bullmq.config';
+import { PaymentModule } from './payment/payment.module';
 import { WalletModule } from './wallet/wallet.module';
 import { TonModule } from './ton/ton.module';
-import { PaymentModule } from './payment/payment.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
     ConfigModule.forRoot(ConfigModuleOptions()),
+    ScheduleModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: BullConfigService
+    }),
     PrismaModule,
     WalletModule,
     TonModule,
     PaymentModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }

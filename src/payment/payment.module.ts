1a6e0862895893a4b440f85dcfdf3fdd45791d0d
milad <miladbachari14@gmail.com>
2024-08-21 13:46:49 +0330

    feat: add payment controller in module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PaymentService } from "./payment.service";
 import { WalletModule } from "src/wallet/wallet.module";
+import { PaymentController } from "./payment.controller";
 @Module({
     imports: [WalletModule],
     providers: [PaymentService],
+    controllers: [PaymentController],
     exports: [PaymentService],
 })
 export class PaymentModule { }
